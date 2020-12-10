710f225016e46e9becbb4531ef0cc7ee2fa54f6a
SmeyersT <67054264+SmeyersT@users.noreply.github.com>
2020-12-10 13:11:04 +0100

    Update globals.dart

--- lib/core/values/globals.dart
+++ lib/core/values/globals.dart
@@ -1,5 +1,3 @@
 const String BASE_URL = 'http://192.168.0.114:9090/api';
 const String SOCKET_URL = 'http://192.168.0.114:9090/api/socket';
 
-// const String BASE_URL = 'http://84.198.6.93:8080/api';
-// const String SOCKET_URL = 'http://84.198.6.93:8080/api/socket';
